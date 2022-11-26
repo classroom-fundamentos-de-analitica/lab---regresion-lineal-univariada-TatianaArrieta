e72a41d523686857bc599c6f73648153465d005c
TatianaArrieta <113394215+TatianaArrieta@users.noreply.github.com>
2022-11-26 12:45:04 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -15,7 +15,7 @@ def pregunta_01():
     Complete el código presentado a continuación.
     """
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv('gm_2008_region.csv', sep=",", thousands=None, decimal=".")
+    df = pd.read_csv('gm_2008_region.csv', sep=",")
 
     # Asigne la columna "life" a `y` y la columna "fertility" a `X`
     y = df["life"].values
@@ -47,7 +47,7 @@ def pregunta_02():
     """
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv('gm_2008_region.csv', sep=",", thousands=None, decimal=".")
+    df = pd.read_csv('gm_2008_region.csv', sep=",")
 
     # Imprima las dimensiones del DataFrame
     print(df.shape)
@@ -72,7 +72,7 @@ def pregunta_03():
     """
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv('gm_2008_region.csv', sep=",", thousands=None, decimal=".")
+    df = pd.read_csv('gm_2008_region.csv', sep=",")
 
     # Asigne a la variable los valores de la columna `fertility`
     X_fertility = df["fertility"].values.reshape(-1,1)df["life"].values.reshape(-1,1)
@@ -117,7 +117,7 @@ def pregunta_04():
     from sklearn.metrics import mean_squared_error
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv('gm_2008_region.csv', sep=",", thousands=None, decimal=".")
+    df = pd.read_csv('gm_2008_region.csv', sep=",")
 
     # Asigne a la variable los valores de la columna `fertility`
     X_fertility = df["fertility"].values.reshape(-1,1)
